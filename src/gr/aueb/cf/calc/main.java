039442395c3d0916a89eb16171ca219adaeddaab
lampros alexandris <lamprospatmos19@gmail.com>
2024-12-19 03:35:21 +0200

    new created

--- src/gr/aueb/cf/calc/main.java
+++ src/gr/aueb/cf/calc/main.java
@@ -2,7 +2,7 @@ package gr.aueb.cf.calc;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println(add(10, 20));
 
     }
     public static int add(int a, int b){
